12d01eb4069cafb55e801e4b77009767f9562b31
fyx1t <Kerry_win>
2024-03-26 19:11:04 +0300

    docs: :memo: update version

--- configs.sh
+++ configs.sh
@@ -1,3 +1,3 @@
 touch .env
 
-echo -e "PORT=22\nALLOW_COMMANDS=true\nCOMMANDS=hostname, ip a\nTHREADS=8\nENTER_TO_CONTINUE=false\nFILENAME_GOOD_IPS=good_ips.txt\nFILENAME_ERRORS=errors.txt\nFILENAME_FOUND_IPS=found.txt\nFILENAME_FOUND_IPS_EXTENDED=found_extended.txt\nFILENAME_LOGS=work.log\nINPUT_FOLDER=data/\nOUTPUT_FOLDER=data/\nVERSION=v0.2" > .env
+echo -e "PORT=22\nALLOW_COMMANDS=true\nCOMMANDS=hostname, ip a\nTHREADS=8\nENTER_TO_CONTINUE=false\nFILENAME_GOOD_IPS=good_ips.txt\nFILENAME_ERRORS=errors.txt\nFILENAME_FOUND_IPS=found.txt\nFILENAME_FOUND_IPS_EXTENDED=found_extended.txt\nFILENAME_LOGS=work.log\nINPUT_FOLDER=data/\nOUTPUT_FOLDER=data/\nVERSION=v0.3" > .env
